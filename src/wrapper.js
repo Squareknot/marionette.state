f97f2d338db425bc0a16258d97b281f734aed3e5
Ian Stewart <ianmstew@gmail.com>
2015-02-17 12:35:11 -0700

    Initial source

---
+++ src/wrapper.js
@@ -0,0 +1,24 @@
+/* global define */
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['backbone', 'marionette', 'underscore'], function (Backbone, Marionette, _) {
+      return factory(Backbone, Marionette, _);
+    });
+  }
+  else if (typeof exports !== 'undefined') {
+    var Backbone = require('backbone');
+    var Marionette = require('marionette');
+    var _ = require('underscore');
+    module.exports = factory(Backbone, Marionette, _);
+  }
+  else {
+    factory(root.Backbone, root.Backbone.Marionette, root._);
+  }
+}(this, function (Bb, Mn, _) {
+  'use strict';
+
+  // @include marionette.state-service.js
+
+  return Mn.StateService;
+}));
