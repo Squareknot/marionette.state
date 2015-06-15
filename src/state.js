6e539adbfe5d28d7ff01ba5296d99414b8d19af6
Ian Stewart <ianmstew@gmail.com>
2015-06-15 16:04:10 -0400

    Make State an entity proxy.

--- src/state.js
+++ src/state.js
@@ -36,51 +36,27 @@ const State = Mn.Object.extend({
     // State model class is either passed in, on the class, or a standard Backbone model
     this.modelClass = options.modelClass || this.modelClass || Backbone.Model;
 
-    this.resetState(options.initialState);
+    // Initialize state
+    this.initState(options.initialState);
 
     State.__super__.constructor.apply(this, arguments);
   },
 
   // Initialize model with attrs or reset it, destructively, to conform to attrs.
-  resetState(attrs, options) {
-    /* eslint-disable new-cap */
+  initState(attrs, options) {
+    // Set initial state.
     this._initialState = _.extend({}, this.defaultState, attrs);
 
-    // If model is set, reset it. Otherwise, create it.
     if (this._model) {
+      // Reset existing model with initial state.
       this.reset(null, options);
-    } else {
-      this._model = new this.modelClass(this._initialState);
     }
-  },
-
-  // Return the state model.
-  getModel() {
-    return this._model;
-  },
-
-  // Returns the initiate state, which is reverted to by reset()
-  getInitialState() {
-    return _.clone(this._initialState);
-  },
-
-  // Return state to its initial value.
-  // If `attrs` is provided, they will override initial values for a "partial" reset.
-  reset(attrs, options) {
-    var resetAttrs = _.extend({}, this._initialState, attrs);
-    this._model.set(resetAttrs, options);
-  },
-
-  // Proxy to model set().
-  set() {
-    if (!this._model) { throw new Mn.Error('Initialize state first.'); }
-    this._model.set.apply(this._model, arguments);
-  },
-
-  // Proxy to model get().
-  get() {
-    if (!this._model) { throw new Mn.Error('Initialize state first.'); }
-    return this._model.get.apply(this._model, arguments);
+    else {
+      // Create new model with initial state.
+      /* eslint-disable new-cap */
+      this._model = new this.modelClass(this._initialState);
+      this._proxyEvents(this._model);
+    }
   },
 
   // Bind lifetime and component events to an object initialized with Backbone.Events, such as
@@ -97,14 +73,71 @@ const State = Mn.Object.extend({
     }
   },
 
+  // Returns the initiate state, which is reverted to by reset()
+  getInitialState() {
+    return _.clone(this._initialState);
+  },
+
   // Marionette object bound to
   getComponent() {
     return this._component;
   },
 
+  // Return the state model.
+  getModel() {
+    return this._model;
+  },
+
+  // Proxy to model set().
+  set() {
+    if (!this._model) { throw new Mn.Error('Initialize state first.'); }
+    this._model.set.apply(this._model, arguments);
+  },
+
+  // Proxy to model get().
+  get() {
+    if (!this._model) { throw new Mn.Error('Initialize state first.'); }
+    return this._model.get.apply(this._model, arguments);
+  },
+
+  // Return state to its initial value.
+  // If `attrs` is provided, they will override initial values for a "partial" reset.
+  reset(attrs, options) {
+    var resetAttrs = _.extend({}, this._initialState, attrs);
+    this._model.set(resetAttrs, options);
+  },
+
+  // Proxy to model changedAttributes().
+  getChanged() {
+    return this._model.changedAttributes();
+  },
+
+  // Proxy to model previousAttributes().
+  getPrevious() {
+    return this._model.previousAttributes();
+  },
+
+  // Determine if any of the passed attributes were changed during the last modification.
+  hasAnyChanged(...attrs) {
+    return !!_.chain(this._model.changed)
+      .keys()
+      .intersection(attrs)
+      .size()
+      .value();
+  },
+
   // Proxy to StateFunctions#syncEntityEvents.
-  syncEntityEvents(entity, entityEvents) {
-    return syncEntityEvents(this, entity, entityEvents);
+  syncEntityEvents(entity, entityEvents, event) {
+    return syncEntityEvents(this, entity, entityEvents, event);
+  },
+
+  _proxyEvents: function (other) {
+    this.listenTo(other, 'all', function () {
+      if (arguments.length > 1 && arguments[1] === this._model) {
+        arguments[1] = this;
+      }
+      this.trigger.apply(this, arguments);
+    }.bind(this));
   }
 });
 
